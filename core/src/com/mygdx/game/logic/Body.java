70509a8130d7186c029a4add9fa45e9605cf2cd8
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-12 08:13:24 -0600

    Created basic body class

---
+++ core/src/com/mygdx/game/logic/Body.java
@@ -0,0 +1,73 @@
+package com.mygdx.game.logic;
+
+/**
+ * Created by fraayala19 on 12/12/17.
+ */
+public class Body {
+    private double mass;
+    private double x, y, z;
+    private double vX, vY, vZ;
+
+    public Body(double mass, double x, double y, double z) {
+        this.mass = mass;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getvX() {
+        return vX;
+    }
+
+    public void setvX(double vX) {
+        this.vX = vX;
+    }
+
+    public double getvY() {
+        return vY;
+    }
+
+    public void setvY(double vY) {
+        this.vY = vY;
+    }
+
+    public double getvZ() {
+        return vZ;
+    }
+
+    public void setvZ(double vZ) {
+        this.vZ = vZ;
+    }
+}
