1a6c367449b13807309390058eee08f2da6e7a31
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-12 08:30:54 -0600

    Set simulation screen by default

--- core/src/com/mygdx/game/Boot.java
+++ core/src/com/mygdx/game/Boot.java
@@ -11,7 +11,12 @@ public class Boot extends Game {
 	
 	@Override
 	public void create () {
+		Gdx.graphics.setResizable(true);
+		Gdx.graphics.setWindowedMode(800,600);
+		Gdx.graphics.setTitle("Orbit Simulator");
+		Gdx.graphics.setVSync(true);
 
+		setScreen(new SimulationScreen(this));
 	}
 
 	@Override
