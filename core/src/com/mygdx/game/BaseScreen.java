f5ac60ef3c0477f6d1f06d7d1fd6a8f9c2888c99
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-12 08:15:54 -0600

    Created basic SimulationScreen class

--- core/src/com/mygdx/game/BaseScreen.java
+++ core/src/com/mygdx/game/BaseScreen.java
@@ -1,9 +1,11 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by fraayala19 on 12/12/17.
  */
-public abstract class BaseScreen {
+public abstract class BaseScreen implements Screen{
     private Boot boot;
 
     public BaseScreen(Boot boot) {
---
+++ core/src/com/mygdx/game/SimulationScreen.java
@@ -0,0 +1,46 @@
+package com.mygdx.game;
+
+/**
+ * Created by fraayala19 on 12/12/17.
+ */
+public class SimulationScreen extends BaseScreen {
+
+    public SimulationScreen(Boot boot) {
+        super(boot);
+    }
+
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
