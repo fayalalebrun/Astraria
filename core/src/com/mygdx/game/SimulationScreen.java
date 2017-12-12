afd248ba8e6807a16770e534e0137e6cfd1dd2ea
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-12 12:15:39 -0600

    Added basic 3d test

--- core/src/com/mygdx/game/SimulationScreen.java
+++ core/src/com/mygdx/game/SimulationScreen.java
@@ -1,21 +1,74 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 /**
  * Created by fraayala19 on 12/12/17.
  */
 public class SimulationScreen extends BaseScreen {
 
+    private PerspectiveCamera cam;
+
+    private ModelBatch modelBatch;
+    private ModelInstance instance;
+    private Model model;
+    private Environment environment;
+
+    public CameraInputController camController;
+
     public SimulationScreen(Boot boot) {
         super(boot);
     }
 
     @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0.4f, 0.5f, 0.8f, 1f);
+    public void show() {
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        modelBatch = new ModelBatch();
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+
+        final Material material = new Material(ColorAttribute.createDiffuse(Color.GREEN));
+        final long attributes = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
+        model = modelBuilder.createSphere(4f, 4f, 4f, 24, 24, material, attributes);
+        instance = new ModelInstance(model);
     }
+
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        cam.update();
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+    }
+
 
     @Override
     public void resume() {
@@ -37,14 +90,11 @@ public class SimulationScreen extends BaseScreen {
 
     }
 
-    @Override
-    public void show() {
-
-    }
 
     @Override
     public void dispose() {
-
+        modelBatch.dispose();
+        model.dispose();
     }
 
 
