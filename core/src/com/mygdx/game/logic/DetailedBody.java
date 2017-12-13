0458e886538f898f6d5f1e1dd8497223c5fc0c50
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-13 11:17:33 -0600

    Added name field to detailed body

--- core/src/com/mygdx/game/logic/DetailedBody.java
+++ core/src/com/mygdx/game/logic/DetailedBody.java
@@ -10,10 +10,20 @@ public class DetailedBody extends Body{
 
     ModelInstance instance;
     protected double radius;
+    protected String name;
 
-    public DetailedBody(double mass, double x, double y, double z, Model model, double radius) {
+    public DetailedBody(double mass, double x, double y, double z, Model model, double radius, String name) {
         super(mass, x, y, z);
         this.instance = new ModelInstance(model);
         this.radius = radius;
+        this.name = name;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getName() {
+        return name;
     }
 }
