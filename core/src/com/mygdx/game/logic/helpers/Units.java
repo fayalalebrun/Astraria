ee018634fd3c156580c5d53cf08922b95bb16a4f
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-13 11:39:15 -0600

    Added Units helper class

---
+++ core/src/com/mygdx/game/logic/helpers/Units.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.logic.helpers;
+
+/**
+ * Created by fraayala19 on 12/13/17.
+ */
+public class Units {
+    public static final double GRAV = 6.67408 * Math.pow(10,-11);
+
+    public static double mToAU(double meters){
+        return meters/149597870691.0;
+    }
+
+    public static double AUToM(double au){
+        return au*149597870691.0;
+    }
+}
