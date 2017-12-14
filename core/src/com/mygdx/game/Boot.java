bbadd117edcf92d7c3212a53c57721976abda46c
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-14 13:35:56 -0600

    Load comic sans

--- core/src/com/mygdx/game/Boot.java
+++ core/src/com/mygdx/game/Boot.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kotcrab.vis.ui.VisUI;
 
@@ -21,7 +22,7 @@ public class Boot extends Game {
 		Gdx.graphics.setVSync(true);
 
 		manager = new AssetManager();
-
+		manager.load("comicsans.fnt", BitmapFont.class);
 		manager.finishLoading();
 
 		VisUI.load();
