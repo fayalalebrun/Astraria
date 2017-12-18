498bc8f482605ce28c5fa9a2810b6d98a66d299b
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2017-12-18 10:41:54 -0600

    Added preliminary drawing of labels on planets

--- core/src/com/mygdx/game/SimulationScreen.java
+++ core/src/com/mygdx/game/SimulationScreen.java
@@ -1,10 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
@@ -14,11 +17,18 @@ import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.utils.Align;
+import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.widget.VisLabel;
 import com.mygdx.game.logic.Body;
 import com.mygdx.game.logic.DetailedBody;
 
+import javax.xml.soap.Detail;
 import java.util.ArrayList;
 import java.util.Vector;
+
+import static com.mygdx.game.Boot.manager;
 
 /**
  * Created by fraayala19 on 12/12/17.
@@ -33,6 +43,9 @@ public class SimulationScreen extends BaseScreen {
     private Environment environment;
     ArrayList<ModelInstance> instances;
     Vector<DetailedBody> bodies;
+    SpriteBatch bodyLabelBatch;
+
+    DetailedBody earth;
 
     private CameraInputController camController;
 
@@ -40,6 +53,7 @@ public class SimulationScreen extends BaseScreen {
         super(boot);
         bodies = new Vector<DetailedBody>();
         instances = new ArrayList<ModelInstance>();
+        bodyLabelBatch = new SpriteBatch();
     }
 
     @Override
@@ -54,8 +68,8 @@ public class SimulationScreen extends BaseScreen {
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.position.set(0.0000000668449198f, 0.0000000668449198f, 0.0000000668449198f);
         cam.lookAt(0,0,0);
-        cam.near = 0f;
-        cam.far = 14960000000000000f;
+        cam.near = 0.00000000001f;
+        cam.far = 1496000000000f;
         cam.update();
 
         camController = new CameraInputController(cam);
@@ -65,9 +79,14 @@ public class SimulationScreen extends BaseScreen {
 
         final Material material = new Material(ColorAttribute.createDiffuse(Color.GREEN));
         final long attributes = VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal;
-        model = modelBuilder.createSphere(0.00000000668449198f, 0.00000000668449198f, 0.00000000668449198f, 24, 24, material, attributes);
-        instance = new ModelInstance(model);
-        instances.add(instance);
+        model = modelBuilder.createSphere(1, 1, 1, 24, 24, material, attributes);
+
+
+
+        earth = new DetailedBody(123,0,0,0,model, 60000000000f,"earth");
+
+        addBody(earth);
+        earth.centerCamera(cam);
 
 
     }
@@ -75,12 +94,34 @@ public class SimulationScreen extends BaseScreen {
 
     @Override
     public void render(float delta) {
+
+
+        for(DetailedBody body : this.bodies){
+            body.render();
+        }
+
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         cam.update();
         modelBatch.begin(cam);
         modelBatch.render(instances, environment);
         modelBatch.end();
+
+        drawLabels(bodyLabelBatch);
+    }
+
+    private void drawLabels(SpriteBatch batch){
+        batch.begin();
+
+        BitmapFont font = manager.get("comicsans.fnt", BitmapFont.class);
+
+        Vector3 labelPos = earth.getPos().cpy();
+
+        cam.project(labelPos);
+        for(DetailedBody body : this.bodies){
+            font.draw(bodyLabelBatch,body.getName(),labelPos.x,labelPos.y);
+        }
+        batch.end();
     }
 
 
