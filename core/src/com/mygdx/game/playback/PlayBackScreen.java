ce0e21c09247b21cfe81cb8199f1fe1aa9e9338a
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-01-11 11:35:00 -0600

    Initialise objects

--- core/src/com/mygdx/game/playback/PlayBackScreen.java
+++ core/src/com/mygdx/game/playback/PlayBackScreen.java
@@ -1,15 +1,38 @@
 package com.mygdx.game.playback;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
+import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
+import com.badlogic.gdx.graphics.g3d.decals.DecalMaterial;
 import com.mygdx.game.BaseScreen;
 import com.mygdx.game.Boot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by fraayala19 on 1/11/18.
  */
 public class PlayBackScreen extends BaseScreen{
 
+    private PerspectiveCamera cam;
+
+    private DecalBatch decalBatch;
+
+    private ArrayList<Decal> toRender;
+
+
     public PlayBackScreen(Boot boot, String[] arg) {
         super(boot);
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        decalBatch = new DecalBatch(new CameraGroupStrategy(cam));
+        toRender = new ArrayList<Decal>();
+        
     }
 
     @Override
@@ -19,7 +42,7 @@ public class PlayBackScreen extends BaseScreen{
 
     @Override
     public void render(float delta) {
-        
+
     }
 
     @Override
@@ -44,6 +67,6 @@ public class PlayBackScreen extends BaseScreen{
 
     @Override
     public void dispose() {
-
+        decalBatch.dispose();
     }
 }
