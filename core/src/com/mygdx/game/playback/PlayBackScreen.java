2ee4846792360eafa07df88ea6ef4b61d7588439
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-01-11 11:18:00 -0600

    Created PlayBackScreen class

---
+++ core/src/com/mygdx/game/playback/PlayBackScreen.java
@@ -0,0 +1,49 @@
+package com.mygdx.game.playback;
+
+import com.mygdx.game.BaseScreen;
+import com.mygdx.game.Boot;
+
+/**
+ * Created by fraayala19 on 1/11/18.
+ */
+public class PlayBackScreen extends BaseScreen{
+
+    public PlayBackScreen(Boot boot, String[] arg) {
+        super(boot);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
