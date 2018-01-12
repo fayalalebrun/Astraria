fef5802cb44ab2ef98b3ee40ddecc00bad89f839
2019 - ROCKENZAHN GALLEGOS, DAVID THOMAS <davrockenzahn19@David-Rockenzahn-Gallegos.local>
2018-01-11 18:00:20 -0600

    Created generator tool

---
+++ Generator tool/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.screen.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Menu menu = new Menu(args);
+        menu.mainMenu();
+
+    }
+}
