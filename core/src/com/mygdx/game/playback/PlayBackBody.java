985279e682a99d18ad07890d9eec0b4911044b87
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2018-01-11 19:04:17 -0600

    Added basic constructor

--- core/src/com/mygdx/game/playback/PlayBackBody.java
+++ core/src/com/mygdx/game/playback/PlayBackBody.java
@@ -1,10 +1,22 @@
 package com.mygdx.game.playback;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
+
+import java.util.ArrayList;
+
 /**
  * Created by fraayala19 on 1/11/18.
  */
 public class PlayBackBody {
-    private float x, y, z;
+    private ArrayList<Vector3> positions;
+    private ModelInstance modelInstance;
 
+    public PlayBackBody(Model model) {
+        modelInstance = new ModelInstance(model);
+    }
 
+    
 }
