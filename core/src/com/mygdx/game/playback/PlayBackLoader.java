d0367f6b7da3868719d862a9954f683b740aea5d
AYALA LEBRUN, FRANCISCO <fraayala19@Francisco-Ayala-Lebrun.local>
2018-02-14 08:38:31 -0600

    Random access initial loading of file

--- core/src/com/mygdx/game/playback/PlayBackLoader.java
+++ core/src/com/mygdx/game/playback/PlayBackLoader.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.playback;
 
 import com.badlogic.gdx.math.Vector3;
+import com.sun.tools.javac.util.Pair;
 
 import java.io.RandomAccessFile;
 import java.util.Vector;
@@ -17,16 +18,28 @@ public class PlayBackLoader implements Runnable {
     private boolean terminate;
 
     private int numberOfBodies;
-    private float bodyScale, minAccel, maxAccel;
+    private float bodyScale, minAccel, maxAccel, cycles;
 
-    private ConcurrentHashMap<Integer, Vector<Vector3>> frameMap;
+    private ConcurrentHashMap<Integer, Vector<Pair<Vector3, Float>>> frameMap;
 
     public PlayBackLoader(String path) {
         this.path = path;
-        
+
 
         try {
             randomAccessFile = new RandomAccessFile(path, "r");
+
+            short version = randomAccessFile.readShort();
+
+            if(version==1){
+                numberOfBodies = randomAccessFile.readInt();
+                bodyScale = randomAccessFile.readFloat();
+                long length = randomAccessFile.length();
+                length-=length - 10 - 2;
+                length/=4;
+                cycles = length;
+            }
+
         }catch (Exception e){
             e.printStackTrace();
         }
