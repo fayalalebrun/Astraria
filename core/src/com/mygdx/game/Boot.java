0fd4be9835f4d2acc4522465083f3bbbf2b4def3
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2018-03-12 21:31:19 -0600

    Modified boot to use OpenGL 3.0

--- core/src/com/mygdx/game/Boot.java
+++ core/src/com/mygdx/game/Boot.java
@@ -1,15 +1,13 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kotcrab.vis.ui.VisUI;
 import com.mygdx.game.playback.PlayBackScreen;
+import com.mygdx.game.simulation.SimulationScreen;
 
 public class Boot extends Game {
 
@@ -39,7 +37,7 @@ public class Boot extends Game {
 		VisUI.load();
 
 		if(arg[0].equals("sim")){
-			setScreen(new SimulationScreen(this, arg[1]));
+			setScreen(new SimulationScreen(this));
 		} else if (arg[0].equals("play")){
 			setScreen(new PlayBackScreen(this,arg[1]));
 		}
