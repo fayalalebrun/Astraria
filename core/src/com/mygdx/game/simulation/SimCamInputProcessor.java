250018196f7b8d84695fe92424ac0cbca8b836e9
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2018-03-17 19:36:32 -0600

    Created class to process input

---
+++ core/src/com/mygdx/game/simulation/SimCamInputProcessor.java
@@ -0,0 +1,75 @@
+package com.mygdx.game.simulation;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.mygdx.game.simulation.renderer.Camera;
+import com.mygdx.game.simulation.renderer.Camera_Movement;
+
+import static com.badlogic.gdx.Gdx.input;
+
+/**
+ * Created by Fran on 3/17/2018.
+ */
+public class SimCamInputProcessor implements InputProcessor{
+
+    Camera camera;
+
+    public SimCamInputProcessor(Camera cam) {
+        this.camera = cam;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode){
+            case Input.Keys.W:
+                camera.processKeyboard(Camera_Movement.FORWARD);
+                return true;
+            case Input.Keys.S:
+                camera.processKeyboard(Camera_Movement.BACKWARD);
+                return true;
+            case Input.Keys.A:
+                camera.processKeyboard(Camera_Movement.LEFT);
+                return true;
+            case Input.Keys.D:
+                camera.processKeyboard(Camera_Movement.RIGHT);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
