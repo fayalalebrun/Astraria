f0f20dce1f843fb589effcc45daf1ac6c9e7b527
DESKTOP-R6B97RF\Fran <frankxlebrun@gmail.com>
2018-03-18 18:17:23 -0600

    Added point light test class

---
+++ core/src/com/mygdx/game/simulation/renderer/PointLight.java
@@ -0,0 +1,37 @@
+package com.mygdx.game.simulation.renderer;
+
+import org.joml.Vector3f;
+
+/**
+ * Created by Fran on 3/18/2018.
+ */
+public class PointLight implements LightEmitter{
+    final Vector3f position, temp;
+
+    public PointLight(float x, float y, float z) {
+        position = new Vector3f(x, y, z);
+        temp = new Vector3f();
+    }
+
+
+    @Override
+    public Vector3f getLightPosition(Camera camera, Transformation transformation) {
+        return transformation.getViewMatrix(camera).transformProject(temp.set(position));
+    }
+
+
+    @Override
+    public Vector3f getAmbient() {
+        return temp.set(0.2f,0.2f,0.2f);
+    }
+
+    @Override
+    public Vector3f getDiffuse() {
+        return temp.set(0.5f,0.5f,0.5f);
+    }
+
+    @Override
+    public Vector3f getSpecular() {
+        return temp.set(1.0f,1.0f,1.0f);
+    }
+}
